1e32d2d3c17dea5c05e1ed3905f46383d312fcba
Voxalice <64228713+Voxalice@users.noreply.github.com>
2021-11-17 03:50:40 +0000

    footer change

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,7 @@
-<footer><h5>This website doesn't collect any information on its users.<br>Created by <a href="//scratch.mit.edu/users/Voxalice">Voxalice</a>, edited by the <a href="//scratch.mit.edu/discuss/topic/508318/">Meower Team</a></h5></footer>
+<footer>
+	<h5>
+		This website doesn't collect any information on its users.<br>
+		Created by <a href="//scratch.mit.edu/users/Voxalice">Voxalice</a>, edited by the <a href="//scratch.mit.edu/discuss/topic/508318/">Meower Team</a><br>
+		WikiMeower is currently unfinished.
+	</h5>
+</footer>
