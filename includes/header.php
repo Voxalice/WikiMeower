7ad09f306342bf823981d87d0d9ada53c7c53c2c
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2021-11-16 22:12:06 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,8 @@
   $top = array(
 		# Random header text
 		'Did you know that Meower currently contains over 9,400 blocks?', 'Fun fact: Meower was once named ScratchConnect!',
-		'The Meower collab has been active since July 2019!',
+		'The Meower collab has been active since July 2019!', 'Remember, don\'t force disconnect from the meower server.',
+	  	'Did you know that Meower\'s server is- wait, I shouldn\'t say that...', 'cake?', 'Facts that you should know: The Meower threads have over 7 Thousand posts!',
 	);
 	$rnd = array_rand($top);
   echo $top[$rnd];
